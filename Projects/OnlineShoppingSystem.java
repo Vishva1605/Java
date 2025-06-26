87dfdf9e686b3667799a8a5bc61a163755d034e3
Vishva1605 <vishvasrinivas123@gmail.com>
2025-06-26 19:54:20 +0530

    Updated Java projects with Scanner and fixes

--- Projects/OnlineShoppingSystem.java
+++ Projects/OnlineShoppingSystem.java
@@ -1,104 +1,136 @@
+import java.util.Scanner;
 
-//E-Commerce Order Management
-class Product{
+// E-Commerce Order Management
+class Product {
     private int id;
     private String name;
     private double price;
     private int quantity;
 
-    public Product(int product_Id,String product_Name,double product_price){
+    public Product(int product_Id, String product_Name, double product_price) {
         this.id = product_Id;
         this.name = product_Name;
         this.price = product_price;
         quantity = 0;
     }
 
-    public int getID(){
+    public int getID() {
         return id;
     }
-    public String getName(){
+
+    public String getName() {
         return name;
     }
-    public double getPrice(){
+
+    public double getPrice() {
         return price;
     }
-    public int getQuantity(){
+
+    public int getQuantity() {
         return quantity;
     }
 
-    public void setQuantity(int quantity){
+    public void setQuantity(int quantity) {
         this.quantity = quantity;
     }
 }
-class Cart{
+
+class Cart {
     private Product[] products;
     private int count;
 
-    public Cart(){
+    public Cart() {
         products = new Product[10];
         count = 0;
     }
-    public int getCount(){
+
+    public int getCount() {
         return count;
     }
-    public Product[] getProducts(){
+
+    public Product[] getProducts() {
         return products;
     }
 
-    void addproduct(Product p,int quantity){
-        if(count<products.length){
+    void addproduct(Product p, int quantity) {
+        if (count < products.length) {
             p.setQuantity(quantity);
             products[count++] = p;
-            System.out.printf("Product '%s' Added to Cart Successfully",p.getName());
-        }else
+            System.out.printf("Product '%s' Added to Cart Successfully", p.getName());
+        } else
             System.out.println("Cart Reached Maximum Products");
         System.out.println();
     }
 
-    void viewcart(){
+    void viewcart() {
         System.out.println("        List of Products in Cart        ");
-        for(int i=0;i<count;i++){
-            System.out.println("Product_Id : "+products[i].getID());
-            System.out.println("Product_name : "+products[i].getName());
-            System.out.println("Product_Price : "+products[i].getPrice());
-            System.out.println("Product_Quantity : "+products[i].getQuantity());
-            System.out.println("Total_Price_of_Product : "+(products[i].getPrice()*products[i].getQuantity()));
+        for (int i = 0; i < count; i++) {
+            System.out.println("Product_Id : " + products[i].getID());
+            System.out.println("Product_name : " + products[i].getName());
+            System.out.println("Product_Price : " + products[i].getPrice());
+            System.out.println("Product_Quantity : " + products[i].getQuantity());
+            System.out.println("Total_Price_of_Product : " + (products[i].getPrice() * products[i].getQuantity()));
             System.out.println("-------------------------------------------------------");
         }
     }
 }
-class Order{
+
+class Order {
     private Cart c;
     private Product[] orderedproducts;
 
-    public Order(Cart c){
+    public Order(Cart c) {
         this.c = c;
         this.orderedproducts = c.getProducts();
     }
 
-    void totalprice(){
+    void totalprice() {
         double price = 0;
-        for(int i=0;i<c.getCount();i++){
+        for (int i = 0; i < c.getCount(); i++) {
             price += orderedproducts[i].getPrice() * orderedproducts[i].getQuantity();
         }
-        System.out.println("Total price In Your Cart is : Rs."+price);
+        System.out.println("Total price In Your Cart is : Rs." + price);
         System.out.println();
     }
 }
+
 public class OnlineShoppingSystem {
     public static void main(String[] args) {
-        Product p1 = new Product(101, "Shampoo", 2);
-        Product p2 = new Product(102, "paste", 10);
-        Product p3 = new Product(103,"brush",20);
+        Scanner sc = new Scanner(System.in);
+
+        // Getting product details via input
+        System.out.print("Enter number of products to add (max 3): ");
+        int n = sc.nextInt();
+
+        Product[] allProducts = new Product[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("\nEnter details for Product " + (i + 1));
+            System.out.print("Enter ID: ");
+            int id = sc.nextInt();
+            sc.nextLine(); // consume newline
+            System.out.print("Enter Name: ");
+            String name = sc.nextLine();
+            System.out.print("Enter Price: ");
+            double price = sc.nextDouble();
+
+            allProducts[i] = new Product(id, name, price);
+        }
 
         Cart myCart = new Cart();
-        myCart.addproduct(p1, 10);
-        myCart.addproduct(p2, 1);
-        myCart.addproduct(p3, 4);
 
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter quantity for " + allProducts[i].getName() + ": ");
+            int qty = sc.nextInt();
+            myCart.addproduct(allProducts[i], qty);
+        }
+
+        System.out.println();
         myCart.viewcart();
 
-        Order ok = new Order(myCart);
-        ok.totalprice();
+        Order order = new Order(myCart);
+        order.totalprice();
+
+        sc.close();
     }
 }
